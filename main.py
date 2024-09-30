661edaec7ce76e92c02278af03cb610b098f308e
matthieu-zozor <matthieu.zozor@edu.esiee.fr>
2024-09-30 11:44:09 +0000

    version finale exo8 lecture de donnée, pytest étrange pour moi, les test mettent une liste de liste au lieu d'une liste comme demandé dans la consigne

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #### Imports et définition des variables globales
-import random
+"""
+importation de csv pour faciliter la lecture du fichier
+"""
+import csv
 
 FILENAME = "listes.csv"
 
@@ -14,34 +17,110 @@ def read_data(filename):
     Returns:
         list: le contenu du fichier (1 list par ligne)
     """
-    l = []
+
+    with open (filename,mode='r',encoding='utf8') as f:
+        r = csv.reader(f,delimiter=';')
+        l = list(r)
     return l
 
 def get_list_k(data, k):
-    l = []
-    return l
+    """
+    retourne la liste d'indice k de data
+
+    Args:
+        data(lst):liste de liste
+        k(int):indice
+    Returns:
+        data[k](list): liste d'indice k
+    """
+    return data[k]
 
 def get_first(l):
-    return None
+    """
+    retourne le premier elt
+
+    Args: 
+        l(list):liste
+
+    Returns :
+        lst[0](int):entier de retour, premier
+    
+    """
+    return l[0]
 
 def get_last(l):
-    return None
+    """
+    retourne le denier elt
+
+    Args: 
+        l(list):liste
+
+    Returns :
+        lst[-1](int):entier de retour, dernier
+    
+    """
+    return l[-1]
 
 def get_max(l):
-    return None
+    """
+    retourne le max
+
+    Args: 
+        l(list):liste
+
+    Returns :
+        maxi(int):max de la liste
+    
+    """
+    maxi=l[0]
+    for elem in l:
+        maxi=max(elem,maxi)
+    return max
 
 def get_min(l):
-    return None
+    """
+    retourne le min
+
+    Args: 
+        l(list):liste
+
+    Returns :
+        mini(int):min de la liste
+    
+    """
+    mini=l[0]
+    for elem in l:
+        mini=min(elem,mini)
+    return mini
 
 def get_sum(l):
-    return None
+    """
+    retourne la somme des elts de l
+
+    Args: 
+        l(list):liste
+
+    Returns :
+        sum(int): la somme des entiers de la lst
+    
+    """
+    som=0
+    for elem in l:
+        som+=int(elem)
+    return som
 
 
 #### Fonction principale
 
 
 def main():
-    pass
+    """
+    teste si la fonction lit bien les données et que les autres fonctions font ce qui est demandé
+    """
+    data=read_data("listes.csv")
+    k=0
+    l=get_list_k(data,k)
+    print(l,get_first(l),get_last(l),get_max(l),get_min(l),get_sum(l))
     # data = read_data(FILENAME)
     # for i, l in enumerate(data):
     #     print(i, l)
